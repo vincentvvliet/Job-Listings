2bfce7ff829780ad854b3e95803f6dfd4744eae1
Vincent van Vliet <vincent.vanvliet01@gmail.com>
2022-07-02 21:35:00 +0200

    Remove unnecessary retrieveData.json file

--- src/retrieveData.js
+++
@@ -1,9 +0,0 @@
-import * as json from '../data.json';
-
-// export default function getData(fileName) {
-//     const toParse = fs.readFileSync(`../${fileName}`, {encoding: 'utf8', flag: 'r'});
-//
-//     return JSON.parse(toParse)
-// }
-
-console.log(json['default']);
